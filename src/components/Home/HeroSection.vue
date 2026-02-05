bc8f14db129b5214f57a2b5e2e0321476809d7d7
EriyaDev <eriyagamedev@gmail.com>
2026-02-05 14:33:09 +0800

    update hero section

--- src/components/Home/HeroSection.vue
+++ src/components/Home/HeroSection.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
-  <div id="hero-section" class="w-full min-h-screen flex pt-40">
-    <div class="container mx-auto">
-      <div class="flex flex-col gap-2 items-center">
+  <div id="hero-section" class="w-full min-h-screen flex pt-40 relative">
+    <div class="container mx-auto z-10">
+      <div class="flex flex-col gap-2 lg:items-center">
         <div
           class="py-2 flex flex-row items-center gap-1 w-fit px-3 text-badge rounded-full border border-text-primary/10 shadow-sm shadow-text-secondary/5"
         >
@@ -12,7 +12,7 @@
         </div>
         <h1 class="text-display text-text-primary font-medium">Build faster. Ship cleaner UI.</h1>
 
-        <p class="text-body text-text-secondary lg:max-w-[50%] text-center">
+        <p class="text-body text-text-secondary lg:max-w-[50%] lg:text-center">
           A production-ready UI component library for React, Vue, and Tailwind. Clean by default,
           fully reusable, and designed to scale with your product.
         </p>
@@ -22,5 +22,7 @@
         </div>
       </div>
     </div>
+    <div class="absolute inset-0 z-0 bg-dashed-line"></div>
+    <div class="absolute inset-0 -z-10 bg-gradient"></div>
   </div>
 </template>
