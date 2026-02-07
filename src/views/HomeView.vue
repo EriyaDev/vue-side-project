e12cba20518d67eaffadf53f057b1a177d744625
EriyaDev <eriyagamedev@gmail.com>
2026-02-07 23:23:41 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AllComponents from '@/components/Home/AllComponents.vue'
+import AllSections from '@/components/Home/AllSections.vue'
 import HeroSection from '@/components/Home/HeroSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <AllComponents />
+  <AllSections />
 </template>
