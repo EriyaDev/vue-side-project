8cc7b125ffb4c29f18da2138c4ba1f6f30cf3c2a
EriyaDev <eriyagamedev@gmail.com>
2026-02-28 21:18:51 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ComponentsView from '@/views/ComponentsView.vue'
+import DocsView from '@/views/DocsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/components',
+      name: 'components',
+      component: ComponentsView,
+    },
+    {
+      path: '/docs',
+      name: 'docs',
+      component: DocsView,
     },
   ],
 })
