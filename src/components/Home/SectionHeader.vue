3e7a2f3670afac7cfaac7d86854c54c393d76496
EriyaDev <eriyagamedev@gmail.com>
2026-02-28 21:18:47 +0800

    update

--- src/components/Home/SectionHeader.vue
+++ src/components/Home/SectionHeader.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 <template>
   <div class="container mx-auto section-header">
     <h1 class="medium-heading font-semibold line-clamp-2">{{ props.title }}</h1>
-    <p class="text-body max-w-[50%] text-center line-clamp-2">
+    <p class="text-body lg:max-w-[50%] text-center line-clamp-2">
       {{ props.description }}
     </p>
   </div>
