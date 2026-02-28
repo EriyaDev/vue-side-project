ddc8ff9edb9f111285d066f0e23bbe8e4b94a83b
EriyaDev <eriyagamedev@gmail.com>
2026-02-28 21:18:30 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="header bg-primary w-full py-4 fixed top-0 left-0 z-50">
+  <div
+    class="header bg-primary w-full py-4 fixed top-0 left-0 z-50 border-b border-b-text-secondary/10"
+  >
     <nav class="container mx-auto flex flex-row items-center justify-between">
       <RouterLink to="/" class="text-lg font-bold"
         ><div class="h-6">
@@ -72,8 +74,9 @@ import { RouterLink, RouterView } from 'vue-router'
       ></RouterLink>
 
       <ul class="flex flex-row items-center space-x-4">
-        <li><RouterLink class="navbar-link" to="">Components</RouterLink></li>
-        <li><RouterLink class="navbar-link" to="">Documentation</RouterLink></li>
+        <li><RouterLink class="navbar-link" to="/">Home</RouterLink></li>
+        <li><RouterLink class="navbar-link" to="/components">Components</RouterLink></li>
+        <li><RouterLink class="navbar-link" to="/docs">Documentation</RouterLink></li>
         <li>
           <a class="navbar-link" href="https://github.com/EriyaDev/vue-side-project"
             ><i class="ri-github-fill text-xl"></i
