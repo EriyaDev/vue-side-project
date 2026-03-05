10821f2dfc4604bb56d1fcf8b2cc7ad2c94f045a
EriyaDev <eriyagamedev@gmail.com>
2026-03-05 23:33:07 +0800

    add button components

--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -6,7 +6,7 @@ import ComponentObject from '@/components/ComponentObject.vue'
   <div class="min-h-screen grid grid-cols-1 lg:grid-cols-[250px_1fr]">
     <!-- Sidebar -->
     <aside class="hidden lg:block bg-gray-50 border-r border-gray-200 p-4 h-full">
-      <nav class="space-y-2 mt-20">
+      <nav class="space-y-2 mt-20 fixed">
         <!-- Navigation items -->
         <h2 class="text-body font-semibold">COMPONENTS</h2>
         <div class="flex flex-col gap-1.5 mt-4">
@@ -68,10 +68,78 @@ import ComponentObject from '@/components/ComponentObject.vue'
         </div>
 
         <component-object
-          :name="'Button'"
+          :name="'Button Primary'"
           :code="`
           <button 
-            class='bg-button-primary text-white font-space-mono text-base  py-3 px-6 rounded-full hover:bg-avocado-1000/75 transition-colors duration-300 hover:cursor-pointer uppercase'>
+            class='bg-black text-white font-space-mono text-base  py-3 px-6 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase'>
+            Button
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button Secondary'"
+          :code="`
+          <button 
+            class='bg-white text-black font-space-mono text-base  py-3 px-6 rounded-full border border-black/20 hover:bg-black/5 transition-all hover:cursor-pointer uppercase'>
+            Button
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button Change Color on Hover'"
+          :code="`
+          <button 
+            class='bg-black text-white font-space-mono text-base border py-3 px-6 rounded-full hover:bg-white hover:text-black  transition-all hover: duration-300 hover:cursor-pointer uppercase'>
+            Button
+          </button>
+
+          <button 
+            class='bg-white text-black font-space-mono text-base border py-3 px-6 rounded-full hover:bg-black hover:text-white  transition-all hover: duration-300 hover:cursor-pointer uppercase'>
+            Button
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button with Icon'"
+          :code="`
+          <button 
+            class='bg-black text-white font-space-mono text-base  py-3 px-6 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase'>
+            <i class='ri-add-circle-fill text-white'></i>
+            Button
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button with Icon Animation'"
+          :code="`
+          <button 
+            class='bg-black text-white font-space-mono text-base py-3 px-6 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase group'>
+            Button
+            <i class='ri-arrow-right-line text-white group-hover:ml-2 transition-all'></i>
+          </button>
+
+          <button 
+            class='relative bg-black text-white font-space-mono text-base py-3 pl-6 pr-12 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase group'>
+            Button
+            <i class='absolute ri-arrow-right-line text-white group-hover:-rotate-45 transition-all right-5'></i>
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button with Shadow Animation'"
+          :code="`
+          <button 
+            class='bg-black shadow-lg shadow-black/20 text-white font-space-mono text-base py-3 px-6 rounded-full hover:bg-black/75 transition-all duration-300 hover:cursor-pointer hover:shadow-none hover:translate-y-1 uppercase'>
+            Button
+          </button>
+
+          <button 
+            class='bg-white shadow-lg shadow-black/20 text-black font-space-mono text-base py-3 px-6 rounded-full border border-black/20 hover:bg-black/5 transition-all hover:cursor-pointer hover:shadow-none hover:translate-y-1 uppercase'>
             Button
           </button>
           `"
