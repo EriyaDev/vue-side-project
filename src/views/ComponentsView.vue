269a40ba1e3a83cb59beeb40cc458f2596c777da
EriyaDev <eriyagamedev@gmail.com>
2026-03-06 11:06:49 +0800

    edit the view

--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ComponentObject from '@/components/ComponentObject.vue'
+import ComponentSection from '@/components/ComponentSection.vue'
 </script>
 
 <template>
@@ -146,6 +147,97 @@ import ComponentObject from '@/components/ComponentObject.vue'
           `"
         ></component-object>
       </div>
+
+      <component-section
+        name="Buttons"
+        description="Reusable, accessible buttons with multiple variants, sizes, and states. Fully responsive
+            and easily customizable using TailwindCSS utility classes for production-ready
+            interfaces."
+      >
+        <component-object
+          :name="'Button'"
+          :code="`
+          <button 
+            class='bg-black text-white font-space-mono text-base  py-3 px-6 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase'>
+            Button
+          </button>
+
+          <button 
+            class='bg-white text-black font-space-mono text-base  py-3 px-6 rounded-full border border-black/20 hover:bg-black/5 transition-all hover:cursor-pointer uppercase'>
+            Button
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button Invert Color on Hover'"
+          :code="`
+          <button 
+            class='bg-black text-white font-space-mono text-base border py-3 px-6 rounded-full hover:bg-white hover:text-black  transition-all hover: duration-300 hover:cursor-pointer uppercase'>
+            Button
+          </button>
+
+          <button 
+            class='bg-white text-black font-space-mono text-base border py-3 px-6 rounded-full hover:bg-black hover:text-white  transition-all hover: duration-300 hover:cursor-pointer uppercase'>
+            Button
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button with Icon'"
+          :code="`
+          <button 
+            class='bg-black text-white font-space-mono text-base  py-3 px-6 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase'>
+            <i class='ri-add-circle-fill text-white'></i>
+            Button
+          </button>
+
+          <button 
+            class='bg-white text-black font-space-mono text-base  py-3 px-6 rounded-full border border-black/20 hover:bg-black/5 transition-all hover:cursor-pointer uppercase'>
+            <i class='ri-add-circle-fill text-black'></i>
+            Button
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button with Icon Animation'"
+          :code="`
+          <button 
+            class='bg-black text-white font-space-mono text-base py-3 px-6 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase group'>
+            Button
+            <i class='ri-arrow-right-line text-white group-hover:ml-2 transition-all'></i>
+          </button>
+
+          <button 
+            class='relative bg-black text-white font-space-mono text-base py-3 pl-6 pr-12 rounded-full hover:bg-black/75 transition-colors duration-300 hover:cursor-pointer uppercase group'>
+            Button
+            <i class='absolute ri-arrow-right-line text-white group-hover:-rotate-45 transition-all right-5'></i>
+          </button>
+          `"
+        ></component-object>
+
+        <component-object
+          :name="'Button with Shadow Animation'"
+          :code="`
+          <button 
+            class='bg-black shadow-lg shadow-black/20 text-white font-space-mono text-base py-3 px-6 rounded-full hover:bg-black/75 transition-all duration-300 hover:cursor-pointer hover:shadow-none hover:translate-y-1 uppercase'>
+            Button
+          </button>
+
+          <button 
+            class='bg-white shadow-lg shadow-black/20 text-black font-space-mono text-base py-3 px-6 rounded-full border border-black/20 hover:bg-black/5 transition-all hover:cursor-pointer hover:shadow-none hover:translate-y-1 uppercase'>
+            Button
+          </button>
+          `"
+        ></component-object>
+      </component-section>
+
+      <component-section
+        name="Cards"
+        description="Responsive card components for organizing content clearly. Built with accessibility in mind and designed for easy reuse and TailwindCSS-driven layout customization."
+      ></component-section>
     </main>
   </div>
 </template>
