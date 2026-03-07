53efad24025a433da31c2434c12c4c782af2761f
EriyaDev <eriyagamedev@gmail.com>
2026-03-07 19:07:47 +0800

    component section

--- src/components/ComponentSection.vue
+++ src/components/ComponentSection.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div id="button" class="flex flex-col gap-6">
+  <div id="button" class="flex flex-col gap-6 mb-20">
     <div class="flex flex-col gap-2">
       <h1 class="medium-heading font-medium">{{ props.name }}</h1>
       <p class="text-body opacity-55 lg:max-w-[60%]">
